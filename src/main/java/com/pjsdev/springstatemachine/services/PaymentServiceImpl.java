504ff108a73251c04581345f2b181808640668ac
pjsdevcom <mail@pjsdev.com>
2021-08-31 10:37:39 +0100

    Add sending events to the State Machine

--- src/main/java/com/pjsdev/springstatemachine/services/PaymentServiceImpl.java
+++ src/main/java/com/pjsdev/springstatemachine/services/PaymentServiceImpl.java
@@ -5,6 +5,8 @@ import com.pjsdev.springstatemachine.domain.PaymentEvent;
 import com.pjsdev.springstatemachine.domain.PaymentState;
 import com.pjsdev.springstatemachine.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    public static final String PAYMENT_ID_HEADER = "payment_id";
 
     private final PaymentRepository paymentRepository;
 
@@ -27,19 +31,34 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTHORISE);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorisePayment(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        sendEvent(paymentId, sm, PaymentEvent.AUTH_APPROVED);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        sendEvent(paymentId, sm, PaymentEvent.AUTH_DECLINED);
+
         return null;
+    }
+
+    private void sendEvent(Long paymentId, StateMachine<PaymentState, PaymentEvent> sm, PaymentEvent event) {
+        Message msg = MessageBuilder.withPayload(event).setHeader(PAYMENT_ID_HEADER, paymentId).build();
+
+        sm.sendEvent(msg);
     }
 
     //to initialize SM from database
